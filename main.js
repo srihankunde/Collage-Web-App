4ffc4ec0cf4e93868f7c1db627dc736a0a39d1b2
srihankunde <90851633+srihankunde@users.noreply.github.com>
2022-03-17 18:51:22 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -10,7 +10,7 @@ function start()
 recognition.onresult = function(event){
     console.log(event);
     var content = event.results[0][0].transcript;
-    document.getElementById("textbox").innerHTML=content;
+   
     console.log(content);
     if(content == " selfie"){
      console.log("taking selfie");
